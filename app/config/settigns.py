e8e84d3928a47012949260668fef516739202ba1
jorge.jaime.dev.io <jorge.jaime.dev.io@gmail.com>
2026-08-19 14:18:24 -0600

    chore: load configuration with pydantic instead of load_dotenv.

--- app/config/settigns.py
+++ app/config/settigns.py
@@ -5,11 +5,22 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file = ".env",
         env_file_encoding = "utf-8",
-        extra = "ignore",
+        extra = "ignore"
     )
 
-    debug: bool = False
-    ollama_host: str = "http://localhost:11434"
-    log_level: str = "INFO"
+    ia_model_host: str = "localhost"
+    ia_model_embedded_name: str = "gpt40-mini"
+    ia_model_agent_name: str = "gpt40-mini"
+    ia_model_api_key: str = ""
+    ia_model_temperature: float = 0.0
+
+    pinecone_host: str = "localhost"
+    pinecone_index_name: str = "lab"
+    pinecone_index_dimensions: int = 768
+    pinecone_index_metric: str = "cosine"
+
+    redis_host: str = "localhost"
+    redis_port: int = 6379
+    redis_db_index: int = 0
 
 settings = Settings()
